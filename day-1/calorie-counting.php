e6ec5ec73a266e32fe8dc871a4d25bfdd87a2668
BrianDGLS <briandgls92@gmail.com>
2023-01-04 14:20:55 +0000

    feat: add day 1 solution

---
+++ day-1/calorie-counting.php
@@ -0,0 +1,24 @@
+<?php
+
+$answer = 0;
+$input = file_get_contents(__DIR__ . '/input.txt');
+$lines = explode(PHP_EOL, $input);
+
+$current = 0;
+$calories = [];
+foreach ($lines as $line) {
+    if (strlen(trim($line)) == 0) {
+        $calories[] = $current;
+        $current = 0;
+    } else {
+        $current += (int)$line;
+    }
+}
+
+sort($calories);
+
+$answer1 = end($calories);
+$answer2 = array_sum(array_slice($calories, -3));
+
+echo 'Answer 1 => ' . $answer1 . PHP_EOL;
+echo 'Answer 2 => ' . $answer2 . PHP_EOL;
